b501a7bc7501970750e0deee992fb3469e754c3d
Lucemans <luc@lucemans.nl>
2020-11-24 17:34:22 +0100

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,89 @@
 import { Subject } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 interface RedisInterface {
     RPUSH: any;
     BLPOP: any;
     RPUSHX: any;
+    SUBSCRIBE: any;
+    UNSUBSCRIBE: any;
+    PUBLISH: any;
+    on: any;
 }
 
-export class StreamClient {
+export interface JobHandler {
+    (input: string): (Promise<string> | string);
+}
 
+export class GenericStream {
     redis: RedisInterface;
-    streams: { [key: string]: Stream } = {};
-
-    constructor(redis: RedisInterface) {
-        this.redis = redis;
-    }
-
-    stream(topic: string): Stream {
-        if (this.stream[topic])
-            return this.stream[topic];
-        this.stream[topic] = new Stream(this, topic);
-        return this.stream(topic);
-    }
-
-}
-
-export class Stream {
-    client: StreamClient;
-    subject: Subject<any>;
     topic: string;
 
-    constructor(client: StreamClient, topic: string) {
-        this.client = client;
+    constructor(redis: RedisInterface, topic: string) {
+        this.redis = redis;
         this.topic = topic;
-        this.subject = new Subject();
     }
 
-    public next(...obj: string[]) {
-        this.client.redis.RPUSH(this.topic, ...obj, (err, n) => {
-            if (err != null)
+    queue(...obj: string[]) {
+        return new Promise<any>((acc, rej) => {
+            this.redis.RPUSH(this.topic, ...obj, (err, value) => {
+                if (err)
+                    rej(err);
+                else
+                    acc(value);
+            });
+        });
+    }
+
+    next() {
+        return new Promise<string>((acc, rej) => {
+            this.redis.BLPOP(this.topic, 0, (err, value) => {
+                if (err)
+                    rej(err);
+                else
+                    acc(value);
+            });
+        });
+    }
+}
+export class JobStream extends GenericStream {
+
+    handleJob(jobHandler: JobHandler) {
+        this.redis.BLPOP(this.topic + "_jobs", 0, async (err, raw_value: string) => {
+            if (err)
                 throw err;
+            // Gather the JOB_ID
+            let job_id = raw_value.split(';')[0];
+            // Extract the payload
+            let value = raw_value.replace(job_id + ';', '');
+            // Execute the handler
+            let v = jobHandler(value);
+            // Await handling
+            if (v instanceof Promise) {
+                v = await v;
+            }
+            // Publish our completed job results
+            this.redis.PUBLISH(this.topic + "_job_" + job_id, v);
         });
     }
 
-    ready() {
-        this.client.redis.BLPOP(this.topic, 0, (err, a) => {
-            this.subject.next(a[1]);
-        });
+    queueJob(payload: string): { job_id: number, job: Promise<string> } {
+        let job_id = uuid().replace('-', '');
+        return {
+            job_id,
+            job: new Promise<string>((acc, rej) => {
+                this.redis.RPUSH(this.topic + "_jobs", job_id + ";" + payload, (err, value) => {
+                    const job_channel = this.topic + "_job_" + job_id;
+                    this.redis.SUBSCRIBE(job_channel);
+
+                    this.redis.on("message", function(channel, message) {
+                        if (channel == job_channel) {
+                            this.redis.UNSUBSCRIBE(job_channel);
+                            acc(message);
+                        }
+                      });
+                });
+            })
+        };
     }
 }
